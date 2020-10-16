35cb3e53e49c4c05ec12393e88f6da99d3451b47
ooladuwa <onaje.oladuwa@gmail.com>
2020-10-16 15:25:45 -0400

    stretched

--- challenges/classes.js
+++ challenges/classes.js
@@ -18,8 +18,25 @@ class CuboidMaker2{
        height: 5
     });
 
+class CubeMaker extends CuboidMaker2{
+    constructor(CubeMakerAtts){
+        super(CubeMakerAtts)
+        };
+    volume() {
+        return Math.pow(this.length, 3);
+    };
+    surfaceArea(){
+        return 6 * (Math.pow(this.length, 2))
+    };
+};
+let cube = new CubeMaker({
+    length: 4,
+    width: 4, 
+    height: 4
+});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
-
+console.log(cube.volume());
+console.log(cube.surfaceArea());
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/closure.js
+++ challenges/closure.js
@@ -23,11 +23,12 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-let summation = function (param){
+let summation = function (num, index){
 let counter = 0;
+// console.log(num);  
 
-  for (let i = 1; i < param; i++){
+for (let i = 1; i <= num; i++){
     return counter += [i];
   };
 };
-console.log(summation(4))
+console.log(summation(4));
