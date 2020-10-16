a005ebf230edfaf529b910cd5e33f57f9aecdad3
ooladuwa <onaje.oladuwa@gmail.com>
2020-10-16 12:38:07 -0400

    working

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,14 @@ myFunction();
 
 // Explanation: 
 
+//It can access the variable internal because it is a closure. That is, while it is inside of the function, it has access to the scope of the outer function.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+let summation = function (param){
+let counter = 0;
+for (let i=0; i<param; i++)
+return counter[i];
+};
+summation()
